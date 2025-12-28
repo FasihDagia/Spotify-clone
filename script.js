ad9732c161f4ccd9ad833bd7553598bb8657cbbd
FasihDagia <fasihdagia2006@gmail.com>
2025-12-29 02:21:17 +0500

    created a function to get the list of songs

---
+++ script.js
@@ -0,0 +1,22 @@
+async function getSongs(){
+    let fet = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text()
+    
+    let div = document.createElement('div');
+    div.innerHTML = response;
+    let as = div.getElementsByTagName('a');
+    let songs = []
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith('.mp3')) {
+            songs.push(element.href);
+        }
+        
+    }
+
+    return songs    
+    
+}
+
+main()
