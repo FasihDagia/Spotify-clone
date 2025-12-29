3cb89b2c23e6e7a3868d9556d2c3eff7b1955391
FasihDagia <fasihdagia2006@gmail.com>
2025-12-30 03:02:42 +0500

    created a function play and pause the song

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let currentSong = new Audio();
+let play = document.querySelector("#play");
+let next = document.querySelector("#next");
+let previous = document.querySelector("#previous");
+
 async function getSongs() {
     let fet = await fetch("http://127.0.0.1:5500/songs/")
     let response = await fet.text()
@@ -10,7 +15,7 @@ async function getSongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith('.mp3')) {
-            songs.push(element.href.split('/songs/')[1]);
+            songs.push(element.href);
         }
 
     }
@@ -19,14 +24,24 @@ async function getSongs() {
 
 }
 
-async function main() {
-    let songs = await getSongs();
+const playMusic = (track) => {
+    let src = "/songs/" + track
+    currentSong.src = src;
+    currentSong.play()
+    play.src = 'img/pause.svg'
+    document.querySelector('.songinfo').innerHTML = track.replace(".mp3","")
+    document.querySelector('.songtime').innerHTML = "00:00/00:00"
+}
 
+
+async function main() {
+
+    let songs = await getSongs();
     let songUL = document.querySelector('.librarysongs').getElementsByTagName('ul')[0];
 
     for (let i = 0; i < songs.length; i++) {
 
-        const element = songs[i];
+        const element = songs[i].split('/songs/')[1];
         songUL.innerHTML = songUL.innerHTML + `<li class="flex">
                             <img src="img/music.svg" alt="music" class="invert">
                             <span class="songDetails">
@@ -39,6 +54,26 @@ async function main() {
                             </div>
                         </li>`
     }
+
+    Array.from(document.querySelector('.librarysongs').getElementsByTagName('li')).forEach((e) => {
+        e.addEventListener('click', () => {
+            playMusic(e.querySelector('.songDetails').firstElementChild.innerHTML+".mp3");
+        })
+    })
+
+    play.addEventListener('click',() => {
+      if(currentSong.paused){
+        currentSong.play()
+        play.src = 'img/pause.svg' 
+      }else{
+        currentSong.pause()
+        play.src = 'img/play.svg'
+      }
+    })
+
+    // next.addEventListener('click',() => {
+    //     currentSong.next
+    // })
 
     var audio = new Audio(songs[0]);
 
