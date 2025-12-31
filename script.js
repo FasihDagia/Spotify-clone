e573bbcebd40547eb7073e5eb37804c16d8c4925
FasihDagia <fasihdagia2006@gmail.com>
2026-01-01 02:44:36 +0500

    added a hamburger feature

--- script.js
+++ script.js
@@ -152,6 +152,15 @@ async function main() {
         }
     })
 
+    document.querySelector("#menu").addEventListener('click',() => {
+      document.querySelector("#left").style.left = "0";
+    })
+
+    document.querySelector("#close").addEventListener('click',() => {
+        document.querySelector("#left").style.left = "-120%";
+    }
+    )
+
 }
 
 main()
